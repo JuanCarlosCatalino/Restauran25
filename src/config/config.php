9598801674b88da5273ca875d184a8a5adcaba55
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2025-11-12 09:35:05 -0500

    tash

--- src/config/config.php
+++ src/config/config.php
@@ -1,14 +1,14 @@
 <?php
 
 const BD_HOST = 'localhost';
-const BD_NAME = 'restaurante2';
+const BD_NAME = 'restaurantes';
 const BD_USER = 'root';
-const BD_PASSWORD = '';
+const BD_PASSWORD = 'root';
 
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
-const BASE_URL = 'http://localhost/restauran25/';
-const BASE_URL_SERVER = 'http://localhost/restauran25/'; 
+const BASE_URL = 'http://localhost:8888/restauran25/';
+const BASE_URL_SERVER = 'http://localhost:8888/restauran25/'; 
 
 ?>
